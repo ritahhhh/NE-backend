272f7d8d53632223256ed2ffdc0efedcf25e4be0
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-04-25 20:36:55 +0200

    login implemented successfully...

--- backend/Routes/loginRoute.js
+++ backend/Routes/loginRoute.js
@@ -8,10 +8,9 @@ router.post("/adminLogin",(req,res)=>{
     const psql='SELECT * FROM admin WHERE email = $1 AND password = $2'
     client.query(psql,[req.body.email, req.body.password],(err,result) => {
         if(err) return res.json({loginStatus: false, Error:"Query error"})
-       
-        if(result.length > 0){
-            
-            const email=result[0].email;
+        
+        if(result.rows.length > 0){
+            const email=result.rows[0].email;
             const token= jwt.sign(
                 {role:"admin", email: email},
                 "jwt_secret_key",
@@ -21,7 +20,9 @@ router.post("/adminLogin",(req,res)=>{
             res.cookie("token",token)
             return res.json({loginStatus: true})
         }else{
+          
             return res.json({loginStatus: false, Error:"Wrong credentials"})
+           
         }
     })
 
