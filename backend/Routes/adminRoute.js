305266fb8f1ab695c38e7ef9c1132d2b21e8bb5a
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-04-30 13:57:27 +0200

    admin add employee routes...

--- backend/Routes/adminRoute.js
+++ backend/Routes/adminRoute.js
@@ -47,11 +47,19 @@ router.get('/category',(req,res)=>{
 })
 
 router.post('/add_employee',(req,res)=>{
-    const psql = 'INSERT INTO employee(name) VALUES($1)';
-    client.query(psql,[req.body.category],(err,result)=>{
-        if(err) return res.json({categoryAddtition: false, Error:"Query error"})
+    const psql = `INSERT INTO employee(fullNames,email,category,phoneNumber) VALUES($1, $2, $3, $4)`;
+    client.query(psql,[req.body.fullName,req.body.email,req.body.category,req.body.telnumber],(err,result)=>{
+        if(err) return res.json({employeeAddition: false, Error:'Query error'})
 
-        return res.json({categoryAddition: true})
+        return res.json({employeeAddition:true, result: result})
+    })
+})
+
+router.get('/employee',(req,res)=>{
+    const psql ="SELECT * FROM employee"
+    client.query(psql,(err,result)=>{
+        if(err) return res.json({status:false, Error:'Query error'})
+        return res.json({status: true, Result: result})
     })
 })
 
